3e95d0cf79909a7dae2522657029c1f8d704135c
Sunhat <Sunhat@users.noreply.github.com>
2023-01-09 07:24:00 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <main>
+    <RouterView></RouterView>
+  </main>
+  <footer></footer>
+</template>
+
+<style scoped>
+
+</style>
